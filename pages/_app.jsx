53e8f29a05603d543699b52c1b547280127a2d9d
Martin Maiaru <tincho_91_15@hotmail.com>
2024-01-11 22:57:52 +0100

    Grit Web

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,13 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import RootLayout from '../layouts/layout'
 import Fonts from 'public/fonts/Fonts.js';
-import theme from './theme';
+
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider >
       <RootLayout>
         <Fonts />
         <Component {...pageProps} />
