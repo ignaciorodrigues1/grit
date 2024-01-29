4ecdd9c9ffa9b6af432cc772f380613ebb0139d2
Martin Maiaru <tincho_91_15@hotmail.com>
2024-01-29 18:02:22 +0100

    Grit Web

--- components/wavyGridProyects.jsx
+++ components/wavyGridProyects.jsx
@@ -11,11 +11,19 @@ import {
 const WavyGridProyects = () => {
   return (
     <Box bgColor="#1D1C1C" overflowX="hidden">
-      <Box maxW="1280px" mx="auto">
-
+      {/* PROYECTO 1 */}
+      <Box
+        maxW="1280px"
+        mx="auto"
+        backgroundImage={{
+          base: "url('/images/heroMobile.png')",
+          md: "url('/images/neanderBg.png')",
+        }}
+        backgroundSize="cover"
+        backgroundPosition="center"
+      >
         <SimpleGrid columns={{ base: 2, md: 4 }} spacing={0}>
-
-          <Box display={{ base: "none", md: "block" }} mr="-10%" >
+          <Box display={{ base: "none", md: "block" }} >
             <Image
               src="/images/proyect1vector.png"
               alt="Image 3"
@@ -31,13 +39,9 @@ const WavyGridProyects = () => {
             mx="auto"
             alignItems="center"
             flexDirection="column"
-          >
-            
-          </Flex>
+          ></Flex>
 
-          {/* Second Image-Information Pair */}
-
-          <Box display={{ base: "none", md: "block" }} mr="-10%" ml="-10%">
+          <Box display={{ base: "none", md: "block" }} >
             <Image
               src="/images/vectorL-R.png"
               alt="Image 3"
@@ -46,7 +50,31 @@ const WavyGridProyects = () => {
             />
           </Box>
 
-          <Box display={{ base: "none", md: "block" }} mr="-10%" ml="-10%" maxH="700px">
+          <Box
+            display={{ base: "none", md: "block" }}
+            mr="-10%"
+            ml="-10%"
+            maxH="700px"
+          >
+            <Image
+              src="/images/vectorC.png"
+              alt="Image 3"
+              h="700px"
+              maxH="90vh"
+            />
+          </Box>
+        </SimpleGrid>
+      </Box>
+
+      {/* PROYECTO 2 */}
+
+      <Box maxW="1280px" mx="auto">
+        <SimpleGrid columns={{ base: 2, md: 4 }} spacing={0}>
+          <Box display={{ base: "none", md: "block" }} >
+
+          </Box>
+
+          <Box display={{ base: "none", md: "block" }} >
             <Image
               src="/images/vectorC.png"
               alt="Image 3"
@@ -55,6 +83,24 @@ const WavyGridProyects = () => {
             />
           </Box>
 
+          <Box display={{ base: "none", md: "block" }} >
+            <Image
+              src="/images/vectorL-R.png"
+              alt="Image 3"
+              h="700px"
+              maxH="90vh"
+            />
+          </Box>
+
+          <Box display={{ base: "none", md: "block" }}>
+            <Image
+              src="/images/proyect2vector.png"
+              alt="Image 3"
+              width="100%"
+              h="700px"
+              maxH="90vh"
+            />
+          </Box>
         </SimpleGrid>
       </Box>
     </Box>
--- pages/proyectos/index.jsx
+++ pages/proyectos/index.jsx
@@ -2,12 +2,16 @@
 
 import { Box } from "@chakra-ui/react";
 import WavyGridProyects from "../../components/wavyGridProyects";
+import TrustUs from "../../components/trustUs";
+import ContactForm from "../../components/contactForm";
 
 const ProyectosPage = () => {
 
   return (
     <Box>
       <WavyGridProyects />
+      <TrustUs />
+      <ContactForm />
     </Box>
   );
 };
