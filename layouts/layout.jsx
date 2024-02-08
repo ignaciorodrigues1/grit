543ad30eaece2422dba11dcb88a74e1aadfa689e
Martin Maiaru <tincho_91_15@hotmail.com>
2024-02-08 19:47:53 +0100

    Grit Web

--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -3,6 +3,7 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import ContactForm from "../components/contactForm";
+import WhatsappIcon from "../components/whatssappIcon";
 
 export default function RootLayout({ children }) {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
         {children}
       <ContactForm/>
       <Footer />
+      <WhatsappIcon />
     </div>
   );
 }
