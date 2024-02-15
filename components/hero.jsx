321eef516e6e230a34beac955e5efacbf074f1ca
Martin Maiaru <tincho_91_15@hotmail.com>
2024-02-15 23:29:16 +0100

    Grit Web

--- components/hero.jsx
+++ components/hero.jsx
@@ -6,49 +6,76 @@ const Hero = () => {
     <Box
       bgColor="#1D1C1C"
       overflowX="hidden"
-      backgroundImage={{
-        base: "url('/images/heroMobile.png')",
-        md: "url('/images/heroDesktop.png')",
-      }}
-      backgroundSize="cover"
-      backgroundPosition="center"
-      h={{ base: "75vh", md: "700px" }}
       position="relative"
+      h={{ base: "75vh", md: "700px" }}
       zIndex="0"
     >
-      <Box maxW="1280px" mx="auto">
-        <Flex
-          maxW="1280px"
-          mx="auto"
-          bottom="0"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          textAlign="center"
-          paddingX={{ base: "2rem", md: "3rem", xl: "0" }}
-          py={10}
-          position="absolute"
-        >
-          <Image
-            src="/images/brand.png"
-            alt="Brand Logo"
-            boxSize={{ base: "120px", md: "180px" }}
-            mt="15%"
-            mb={10}
-          />
+      {/* Imagen de fondo */}
+      <Box
+        as="img"
+        src="/images/heroMobile.png"
+        alt="Background Image"
+        display={{ base: "block", md: "none" }}
+        w="100%"
+        h="100%"
+        objectFit="cover"
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex="-1"
+      />
 
-          <Heading
-            color="white"
-            fontSize={{ base: "20px", sm: "6vw", md: "55px", lg: "72px" }}
-            fontWeight="bold"
-            fontFamily="Travels"
-            textTransform="uppercase"
-            lineHeight={{ base: "25px", sm: "6vw", md: "55px", lg: "67px" }}
-            textShadow="0px 0px 12px rgba(0, 0, 0, 0.65)"
-          >
-            Desarrollamos comunidades sustentables
-          </Heading>
-        </Flex>
+      {/* Video de fondo */}
+      <Box
+        as="video"
+        src="/images/heroVideo.mp4"
+        type="video/mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        w="100%"
+        h="100%"
+        objectFit="cover"
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex="-1"
+        playbackRate={0.5}
+      />
+
+      <Flex
+        maxW="1280px"
+        mx="auto"
+        bottom="0"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        paddingX={{ base: "2rem", md: "3rem", xl: "0" }}
+        py={10}
+        position="absolute"
+      >
+        <Image
+          src="/images/brand.png"
+          alt="Brand Logo"
+          boxSize={{ base: "120px", md: "180px" }}
+          mt="15%"
+          mb={10}
+        />
+
+        <Heading
+          color="white"
+          fontSize={{ base: "20px", sm: "6vw", md: "55px", lg: "72px" }}
+          fontWeight="bold"
+          fontFamily="Travels"
+          textTransform="uppercase"
+          lineHeight={{ base: "25px", sm: "6vw", md: "55px", lg: "67px" }}
+          textShadow="0px 0px 12px rgba(0, 0, 0, 0.65)"
+        >
+          Desarrollamos comunidades sustentables
+        </Heading>
+      </Flex>
 
         <SimpleGrid
           columns={{ base: 2, md: 4 }}
@@ -88,7 +115,6 @@ const Hero = () => {
           </ImageAnimation>
         </SimpleGrid>
       </Box>
-    </Box>
   );
 };
 
